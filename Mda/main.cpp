a926c5de6a836d8a3b85837e8e1f52188d8c27e0
Kokoshaur <kokoshaur-10@mail.ru>
2020-11-14 07:21:07 +0300

    Осталось доделать вывод

--- Mda/main.cpp
+++ Mda/main.cpp
@@ -1,11 +1,11 @@
 #include "Generator.cpp"
 #define ONE_STEP 800
 
-int main(int argc, char* argv[])//кокошар лошар
+int main(int argc, char* argv[])
 {
 	Generator r("Test.wav");
 
-	short* buffer = r.LoadWave("3.wav");
+	unsigned short* buffer = r.LoadWave("4.wav");
 	r.SaveWave(buffer);
 
 	r.SaveByStep(buffer, ONE_STEP, 10);
